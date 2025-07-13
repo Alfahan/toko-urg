b56a5bd157d816c627bef59611c7913e2d6508d2
Alfahan <ali.farhan160@gmail.com>
2025-07-13 09:50:42 +0700

    release: toko-urg

--- resources/views/print/nota_transaction.blade.php
+++ resources/views/print/nota_transaction.blade.php
@@ -123,13 +123,13 @@
     <div class="content">
         <div class="title" style="padding-bottom: 13px">
             <div style="text-align: center;text-transform: uppercase;font-size: 15px">
-                Toko Ramadhan
+                Toko Urg
             </div>
             <div style="text-align: center">
-                Alamat: Jalan Gono Tirtowidjoyo No 678 Dusun II Kp Kebon Kalapa RT 007/004 DS Kutapohaci, Ciampel
+                Alamat: Jl. Urang No.53 Rt/Rw: 003/018
             </div>
             <div style="text-align: center">
-                Telp: 085285589396
+                Telp: 08993613408
             </div>
         </div>
 
